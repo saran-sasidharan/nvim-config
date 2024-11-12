5f335408ac186163b70addb2e8e66765108b6a31
saran-sasidharan <saran.s.dhar@gmail.com>
2024-11-12 11:43:44 +0530

    text highlight on yank

--- lua/saran/init.lua
+++ lua/saran/init.lua
@@ -2,3 +2,14 @@ require("saran.remap")
 require("saran.packer")
 require("saran.set")
 
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
